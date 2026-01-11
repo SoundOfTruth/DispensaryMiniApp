9b0b17a25107bd96c8ead3cce986d7d63348011c
SoundOfTruth <catastrophist@mail.ru>
2026-01-12 00:40:57 +0600

    types redesign

--- frontend/src/types/doctors.ts
+++ frontend/src/types/doctors.ts
@@ -7,47 +7,61 @@ export interface BaseDoctor {
   firstname: string;
   lastname: string;
   middlename: string;
-  fullname?: string;
 }
 
-interface DoctorInfo {
-  speciality: string;
-  qualification: string;
+interface InputDoctorList extends BaseDoctor {
+  qualification: string | null;
+  speciality: string | null;
   department: string;
+}
+
+interface InputDoctor extends InputDoctorList {
+  experience: number;
   education: string[];
   extra_education: string[];
 }
 
-interface InputDoctor extends DoctorInfo {
-  id: number;
-  firstname: string;
-  lastname: string;
-  middlename: string;
-  experience: number;
+export interface SimpleDoctor extends BaseDoctor {
+  fullname: string;
+  qualification: string | null;
+  speciality: string | null;
+  department: string;
 }
 
-export interface Doctor extends BaseDoctor, DoctorInfo {
+export interface Doctor extends SimpleDoctor {
   experience: string;
+  education: string[];
+  extra_education: string[];
 }
 
-export const getDoctorPayload = (doctor: BaseDoctor | undefined): BaseDoctor | undefined => {
-  if (!doctor){
-    return undefined
+function getYearsWord(years: number | undefined): string {
+  if (!years) {
+    return "";
   }
+  if (years % 10 === 1 && years % 100 !== 11) {
+    return `${years}год`;
+  } else if (
+    years % 10 >= 2 &&
+    years % 10 <= 4 &&
+    (years % 100 < 10 || years % 100 >= 20)
+  ) {
+    return `${years}года`;
+  } else {
+    return `${years} лет`;
+  }
+}
+
+export const getComputedDoctor = (doctor: InputDoctor): Doctor => {
   return {
     ...doctor,
     fullname: `${doctor.lastname} ${doctor.firstname} ${doctor.middlename}`,
+    experience: getYearsWord(doctor.experience),
   };
 };
 
-export const getDoctorsPayload = (data: InputDoctor[]): Doctor[] => {
+export const getComputedDoctors = (data: InputDoctorList[]): SimpleDoctor[] => {
   return data.map((doctor) => ({
     ...doctor,
     fullname: `${doctor.lastname} ${doctor.firstname} ${doctor.middlename}`,
-    experience: `${doctor.experience} лет`,
   }));
-};
-
-export const getDoctorsJsonPayload = (): Doctor[] => {
-  return getDoctorsPayload(doctorsJson);
 };
