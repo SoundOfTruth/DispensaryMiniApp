badcd642293c83f9685b5bc2d0bf61ae7c81fd67
SoundOfTruth <catastrophist@mail.ru>
2026-03-04 00:56:27 +0600

    added lazy imports

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,28 +1,47 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-import IndexView from "../views/IndexView.vue";
-import DoctorsView from "../views/DoctorsView.vue";
-import DoctorView from "../views/DoctorView.vue";
-import InspectionsView from "../views/InspectionsView.vue";
-import InspectionView from "../views/InspectionView.vue";
-import AdminView from "../views/AdminView.vue";
-import NotFoundView from "../views/NotFoundView.vue";
-import EquipmentsView from "../views/EquipmentsView.vue";
-
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/admin", name: "admin", component: AdminView },
-    { path: "/", name: "index", component: IndexView },
-    { path: "/doctors", name: "doctors", component: DoctorsView },
-    { path: "/doctors/:doctorId(\\d+)", name: "doctor", component: DoctorView },
-    { path: "/inspections", name: "inspections", component: InspectionsView },
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("../views/AdminView.vue"),
+    },
+    {
+      path: "/",
+      name: "index",
+      component: () => import("../views/IndexView.vue"),
+    },
+    {
+      path: "/doctors",
+      name: "doctors",
+      component: () => import("../views/DoctorsView.vue"),
+    },
+    {
+      path: "/doctors/:doctorId(\\d+)",
+      name: "doctor",
+      component: () => import("../views/DoctorView.vue"),
+    },
+    {
+      path: "/inspections",
+      name: "inspections",
+      component: () => import("../views/InspectionsView.vue"),
+    },
     {
       path: "/inspections/:inspectionId(\\d+)",
       name: "inspection",
-      component: InspectionView,
+      component: () => import("../views/InspectionView.vue"),
     },
-    { path: "/equipments", name: "equipments", component: EquipmentsView },
-    { path: "/:pathMatch(.*)*", name: "notFound", component: NotFoundView },
+    {
+      path: "/equipments",
+      name: "equipments",
+      component: () => import("../views/EquipmentsView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notFound",
+      component: () => import("../views/NotFoundView.vue"),
+    },
   ],
 });
